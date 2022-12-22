9b53ef4816fea689173aedb7f9d74ea9182099c4
AmineZouitine <amine.zouitinepro@gmail.com>
2022-12-22 13:57:13 +0100

    add tauri application

---
+++ front-end/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="h-screen w-screen bg-black">
+      <h2 className="text-red-400">Hi awdthere</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ front-end/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{html,js}",
+    "./components/**/*.{html,js}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
